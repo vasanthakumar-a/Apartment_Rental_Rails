cb79ebc8dd93c1a258f35adfafa9c7b8b6e6caf8
Vasanthakumar <vasanthakumara117@gmail.com>
2022-10-11 11:35:52 +0530

    added basic validation for creating apartments

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,2 +1,8 @@
 class Apartment < ApplicationRecord
+  validates :apartment_name, presence: true
+  validates :location, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :review, presence: true
+  validates :room_size, presence: true
 end
