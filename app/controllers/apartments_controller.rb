ddc57fa91ee821886a35808e64f9b1de41ef3a8d
Vasanthakumar <vasanthakumara117@gmail.com>
2022-10-11 11:14:01 +0530

    functions : index, show, new, create has been added to Apartment Controller

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,4 +1,28 @@
 class ApartmentsController < ApplicationController
   def index
+    @apartments = Apartment.all
+  end
+
+  def show
+    @apartment = Apartment.find(params[:id])
+  end
+
+  def new
+    @apartment = Apartment.new
+  end
+
+  def create
+    @apartment = Apartment.new(apartment_params)
+
+    if @apartment.save
+      redirect_to @apartment
+    else
+      render :new
+    end
+  end
+
+  private
+  def apartment_params
+    params.require(:apartment).permit(:apartment_name, :location, :description, :price, :review, :room_size, :ac, :tv, :wifi, :cctv, :house_keeping)
   end
 end
