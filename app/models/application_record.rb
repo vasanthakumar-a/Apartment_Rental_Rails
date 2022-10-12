add5513d2c069e33c02999547dc623de1f350de9
Vasanthakumar <vasanthakumara117@gmail.com>
2022-10-12 15:50:40 +0530

    modified searching by location

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  scope :filter_by_starts_with, -> (apartment_name) { where("name like ?", "#{apartment_name}%")}
+  scope :filter_by_starts_with, -> (location) { where("name like ?", "#{location}%")}
 
 end
