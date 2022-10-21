986fe9cacfbeb352d88404402a67bfa95920011b
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2022-10-21 13:51:03 +0530

    New two migrations added Field for name,phone number created for both user and owner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def authenticated_user_or_owner?
     user_signed_in? or owner_signed_in?
   end
+  
+  protected 
 
+  def configure_permitted_parameters 
+    devise_parameter_sanitizer.permit(:owner_update, keys: [:name, :email, :phonenumber, :credits, :password, :remember_me])
+    devise_parameter_sanitizer.permit(:user_update, keys: [:name, :email, :phonenumber, :credits, :password, :remember_me])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :phonenumber, :credits, :password, :remember_me])
+  end
 end
