6a3598f429defcbdd6930b42aaaa08e10dbbc94b
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2022-10-26 16:43:37 +0530

    Owner page visibility details changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_26_080157) do
+ActiveRecord::Schema.define(version: 2022_10_26_103307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
