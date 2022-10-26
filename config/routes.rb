b12c5b2616422c2b998c57bfe4384e31b8e48d6a
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2022-10-26 17:06:38 +0530

    Error page is designed for no match routing

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,7 @@ Rails.application.routes.draw do
   resources :apartments do
     get 'page/:page', action: :index, on: :collection
   end
+
+  match "/404", to: "errors#not_found",via: :all 
+  match "/500", to: "errors#internal_server_error",via: :all 
 end
