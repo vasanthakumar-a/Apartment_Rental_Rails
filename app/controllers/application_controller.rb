8c39eca67c59c40ff5cf1c42fe824baf73ce02db
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2022-10-26 10:50:15 +0530

    Profile picture feature is added for owners

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   protected 
 
   def configure_permitted_parameters 
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :phonenumber, :credits, :password])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :phonenumber, :credits, :password, :remember_me])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :phonenumber, :credits, :password, :profile_image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :phonenumber, :credits, :password, :remember_me, :profile_image])
   end
 end
