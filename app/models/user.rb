aab194ed80a6e921deb0a5d14edfa5bb1a72aba1
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2022-10-27 10:45:29 +0530

    Validation for owner and user registration is added.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
+  validates :name, presence: :true
+  validates_length_of :phonenumber, minimum: 10, maximum: 10, allow_blank: false
+  validates_uniqueness_of :email, presence: :true
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
