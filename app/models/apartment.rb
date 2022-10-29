746ea0802f8d4b7d49e69dd6e530e5661d91b0e5
mithun30052001 <lmithun01@gmail.com>
2022-10-29 23:11:46 +0530

    bedrooms,bathroom,building_age and balcony fields changed to number fields so that only integers can be given

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -17,7 +17,7 @@ class Apartment < ApplicationRecord
   validates :balcony, presence: :true
   validates :furnishing_status, presence: :true
   validates :property_type, presence: :true
-  validates :posted_date, presence: :true  
+  validates :posted_date, presence: :true    
 
   validates_numericality_of   :bedrooms    
   validates_numericality_of   :bathrooms 
@@ -27,4 +27,28 @@ class Apartment < ApplicationRecord
   scope :filter_by, -> (location) { where("location ilike ?", "%#{location}%")}
   scope :filter_by_search_location, -> (search_location) { where("location ilike ?", "%#{search_location}%")}
   scope :filter_by_owner_id, -> (owner_id) { where owner_id: owner_id }
+
+  # validate :upload_is_image
+
+  # private  
+
+  # def upload_is_image
+  #   unless upload and upload.content_type =~ /^image\/(jpeg|pjpeg|gif|png|bmp)$/
+  #     errors.add(:upload, "Not a valid image")
+  #   end
+  # end
+
+  # validate :image_type 
+
+  # private 
+  # def image_type
+  #    if images_attached? == false 
+  #      errors.add(:images, "are missing")
+  #    end 
+
+  #    images.each do |image| 
+  #     if !image.content_type.in?(%('image/jpg image/png'))
+  #       errors.add(:images, 'needs to be a valid type')
+  #     end 
+  # end  
 end
