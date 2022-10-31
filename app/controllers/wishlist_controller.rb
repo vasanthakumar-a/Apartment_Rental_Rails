e408ab9794dcc86fab170728d60169f89bbb820c
Vasanthakumar <vasanthakumara117@gmail.com>
2022-10-31 21:57:11 +0530

    Renamed @my_token to @wishlists

--- app/controllers/wishlist_controller.rb
+++ app/controllers/wishlist_controller.rb
@@ -2,42 +2,32 @@ class WishlistController < ApplicationController
 
   def index
     if user_signed_in? or owner_signed_in?
-    @my_token = Wishlist.filter_by_apartment_owner(current_owner.id) if owner_signed_in?
-    @my_token = Wishlist.filter_by_apartment_user(current_user.id) if user_signed_in?
-
-    @apartments = []
-
-    @my_token.each do |token|
-      @apartment = Apartment.find(token.apartment_id)
-      @apartments<<@apartment
+      @wishlists = Wishlist.filter_by_apartment_owner(current_owner.id) if owner_signed_in?
+      @wishlists = Wishlist.filter_by_apartment_user(current_user.id) if user_signed_in?
+    else
+      redirect_to user_session_path
     end
-  else
-    redirect_to user_session_path
-  end
   end
 
   def show
-    wishlist_delete()
-    redirect_to wishlist_index_path and return
+    if wishlist_delete()
+      redirect_to wishlist_index_path and return
+    else
+      redirect_to user_session_path and return
+    end
   end
 
   def wishlist_delete
     @wish = Wishlist.find(params[:id])
-    @wish.destroy
+    @wish_user_id = (@wish.user_id == current_user.id) if user_signed_in?
+    @wish_user_id = (@wish.owner_id == current_owner.id) if owner_signed_in?
+    if @wish_user_id
+      @wish.destroy
+    end
   end
 
   def add
-    @my_token = Wishlist.filter_by_apartment_owner(current_owner.id) if owner_signed_in?
-    @my_token = Wishlist.filter_by_apartment_user(current_user.id) if user_signed_in?
-
     @wishlist = Wishlist.new()
-
-    @my_token.each do |owner_wishlist|
-      if ( owner_wishlist.apartment_id ==  params[:id].to_i )
-        redirect_to root_path and return
-      end
-    end
-
     @wishlist.user_id = current_user.id if user_signed_in?
     @wishlist.owner_id = current_owner.id if owner_signed_in?
     @wishlist.apartment_id = params[:id]
@@ -45,13 +35,9 @@ class WishlistController < ApplicationController
     redirect_back(fallback_location: root_path)
   end
 
-  def destroy
-    puts "####################################"
-  end
-
   private
 
   def apartment_params
-    params.require(:apartment).permit(:apartment_name, :location,)
+    params.require(:apartment).permit(:apartment_name, :location)
   end
 end
