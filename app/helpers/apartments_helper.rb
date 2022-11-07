ff85bc6aaf8ef8726c432468da3b41f596e0ce41
Vasanthakumar <vasanthakumara117@gmail.com>
2022-11-07 22:35:32 +0530

    navbar refactored and designed added profile_image to user added mdb css and js using npm added function in helper

--- app/helpers/apartments_helper.rb
+++ app/helpers/apartments_helper.rb
@@ -1,2 +1,43 @@
 module ApartmentsHelper
+
+  def edit_profile
+    if user_signed_in?
+      edit_user_registration_path
+    elsif owner_signed_in?
+      edit_owner_registration_path
+    else
+    end
+  end
+
+  def authenticated_user_or_owner?
+    user_signed_in? or owner_signed_in?
+  end
+
+  def image_attached?
+    if user_signed_in?
+      current_user.profile_image.attached?
+    elsif owner_signed_in?
+      current_owner.profile_image.attached?
+    else
+      false
+    end
+  end
+
+  def profile_image
+    if user_signed_in?
+      current_user.profile_image
+    elsif owner_signed_in?
+      current_owner.profile_image
+    else
+    end
+  end
+
+  def current_credits
+    if user_signed_in?
+      current_user.credits
+    elsif owner_signed_in?
+      current_owner.credits
+    else
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   validates :name, presence: :true
   validates_length_of :phonenumber, minimum: 10, maximum: 10, allow_blank: false
   validates_uniqueness_of :email, presence: :true
-  
+
+  has_one_attached :profile_image
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
